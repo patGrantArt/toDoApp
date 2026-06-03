dbe4c2cf8046874983bc7e4304f5a44f4bd30a24
patgrantart <patgrantart@gmail.com>
2026-06-04 06:47:43 +1000

    updates to image feature

--- netlify/functions/bgImages.js
+++ netlify/functions/bgImages.js
@@ -1,24 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.handler = async () => {
-    const images = [
-        '/images/Kjetil_Golid_CrossHatchAutomata.png',
-        '/images/Kjetil_Golid_Invade1.jpg',
-        '/images/edward_burtynsky_10.jpg',
-        '/images/edward_burtynsky_11.jpg',
-        '/images/edward_burtynsky_12.jpg',
-        '/images/edward_burtynsky_13.jpg',
-        '/images/edward_burtynsky_14.jpg',
-        '/images/edward_burtynsky_15.jpg',
-        '/images/edward_burtynsky_3.jpg',
-        '/images/edward_burtynsky_4.jpg',
-        '/images/edward_burtynsky_5.jpg',
-        '/images/edward_burtynsky_6.jpg',
-        '/images/edward_burtynsky_7.jpg',
-        '/images/edward_burtynsky_8.jpg',
-        '/images/edward_burtynsky_9.jpg'
-    ];
+    const imagesDir = path.join(__dirname, '../../public/images');
+    const files = fs.readdirSync(imagesDir);
+    const images = files
+        .filter(f => /\.(jpg|jpeg|png|gif|webp|svg)$/i.test(f))
+        .map(f => `/images/${f}`);
+
     return {
         statusCode: 200,
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(images)
     };
 };
+
+
+
+ 
